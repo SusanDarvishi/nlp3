438c33464f0146e8a0dd057a089224121187aedc
Julie <cestarojulie@gmail.com>
2018-10-06 22:07:37 -0400

    calculate part of probabilities
    using new refactored method, working on calculating probabilities.....again.....

--- HMMAlgo.py
+++ HMMAlgo.py
@@ -48,78 +48,10 @@ A[positionOfStart][0] = 1
 lastGenerated = {"start":1}
 tempLastGenerated = {}
 
-#print "----------------------------------------------------------------------------------------"
-
-# #look at each word
-# for word in totalWords:
-# 	#print "word ", word
-# 	#look at all of the parts of speech that can apply to that word
-# 	probWordAndFollowing = 0.0
-# 	probForEachPOSOfWord = {}
-# 	for pos in wordDict.get(word):
-# 		#print "pos ", pos
-
-# 		#look at the probability that each part of speech follows the previous parts of speech
-# 		probFollowingPrevPos = 0.0
-# 		for key in lastGenerated:
-# 			#print "prevpos ", key
-# 			#here we will look at each prev pos that may preceed the pos of the word we're looking at
-# 			followingProbabilities = posDict.get(pos).previousPOS
-# 			#print followingProbabilities
-# 			probFollowingPrevPos = float(followingProbabilities.get(key))
-# 			# print pos
-# 			# print prevpos
-# 			#print "probFollowingPrevPos ", probFollowingPrevPos
-		
-# 		#now we have the probability that the pos we're looking at follows one of the previous pos
-# 		#now we want to multiply that by the probability that the pos is a given word
-# 		wordOccurances = wordDict.get(word).get(pos)
-# 		totalOccurances = posDict.get(pos).numOccurances
-# 		probPOSIsWord = float(wordOccurances)/float(totalOccurances)
-# 		# print wordOccurances
-# 		# print totalOccurances
-# 		#print "probPOSISWord ", probPOSIsWord
-# 		probWordAndFollowing = float(probPOSIsWord * probFollowingPrevPos)
-
-# 		#account for the duplicate probabilities for each pos because of preceeding pos
-# 		if pos in probForEachPOSOfWord.keys():
-# 			#this will allow us to make sure that we are keeping the maximum probability for each pos of each word
-# 			probForEachPOSOfWord[pos] = max(probForEachPOSOfWord[pos], probWordAndFollowing)
-# 		else:
-# 			probForEachPOSOfWord[pos] = probWordAndFollowing
-
-# 	print word
-# 	print "initial probs ", probForEachPOSOfWord
-# 	#embed()
-# 	#now need to account for the fact that this word is following the previous pos included in lastGenerated
-# 	#so we have to multiply these probabilities by those probabilities
-
-# 	firstIterationOfLoop = True
-# 	for prevpos in lastGenerated.keys():
-# 		for currentpos in probForEachPOSOfWord.keys():
-# 			#embed()
-# 			if firstIterationOfLoop:
-# 				print "multiplying ", lastGenerated.get(prevpos), " by ", probForEachPOSOfWord.get(currentpos)
-# 				probForEachPOSOfWord[currentpos] = lastGenerated.get(prevpos) * probForEachPOSOfWord.get(currentpos)
-# 				print "no choice in number kept ", probForEachPOSOfWord.get(currentpos)
-# 			else:
-# 				print "multiplying ", lastGenerated.get(prevpos), " by ", probForEachPOSOfWord.get(currentpos)
-# 				probForEachPOSOfWord[currentpos] = max(probForEachPOSOfWord.get(currentpos),(lastGenerated.get(prevpos) * probForEachPOSOfWord.get(currentpos)))
-# 				print "choosing ", max(probForEachPOSOfWord.get(currentpos),probForEachPOSOfWord.get(currentpos))
-# 		print "currentpos prob ", probForEachPOSOfWord.get(currentpos)
-# 		firstIterationOfLoop = False	
-
-# 	print "final probs ", probForEachPOSOfWord
-
-# 	#print "----------------------------------------------------------------------------------------"
-# 	#now we have the probability for each pos of the word
-# 	#we need to add those to last generated so the following word can account for these probabilities
-# 	lastGenerated = probForEachPOSOfWord
-# 	tempLastGenerated = {}
-
 #begin by looking at each word
 for word in totalWords:
 
+	probEachPOSFollowingPrev = {}
 	#look at all the pos that can preceed that word
 	for prevpos in lastGenerated.keys():
 
@@ -128,8 +60,27 @@ for word in totalWords:
 		for pos in wordDict.get(word).keys():
 			#and then we confirm that it is a pos that can follow the previous pos
 			if prevpos in posDict.get(pos).previousPOS.keys():
-				#something
+				#once we've confirmed, we can begin calculations
+				#first calculate the probability that prevpos preceeds pos -- this is equal to the probability that this pos follows
+				#prevpos times the probability calculated for prevPos
+				probPrevposPreceeds = posDict.get(pos).previousPOS.get(prevpos) * lastGenerated.get(prevpos)
+				
+				#add to dictionary in wider scope
+				if pos in probEachPOSFollowingPrev.keys():
+					#if that pos is already in that dictionary, we want to take the maximum
+					probEachPOSFollowingPrev[pos] = max(probPrevposPreceeds, probEachPOSFollowingPrev.get(pos))
+				else:
+					#if not, just add it
+					probEachPOSFollowingPrev[pos] = probPrevposPreceeds
 			#else we will not calculate probability because there is no chance that the pos can follow the prevpos
+
+
+
+
+
+
+
+
 
 
 
